f5d0656200abb66a2f117b88e742ecc74e831c8e
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-03 13:39:06 +0200

    execute array bonus 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,14 +17,14 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-// const longNames = [];
-// for (let i = 0; i < teachers.length; i++) {
-//   if (teachers[i].length >= 5) {
-//     longNames.push(teachers[i]);
-//   }
-// }
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
 
-// console.log(longNames);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 // teachers.splice(teachers.indexOf("Ed"), 1);
