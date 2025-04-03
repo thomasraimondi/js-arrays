80ab2a69e885c6d2774d437fce82550cdcc3b708
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-03 13:21:31 +0200

    execute array 3

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -1,4 +1,5 @@
 const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
+console.log(teachers);
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
@@ -6,14 +7,18 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = "Patrick";
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(lastTeacher);
+console.log(`Ultimo insegnante rimosso: ${lastTeacher}`);
 console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+// const firstTeacher = teachers.shift();
+// console.log(`Primo insegnante rimosso: ${firstTeacher}`);
+// console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
