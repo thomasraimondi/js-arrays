b474a985f55147b09bd59f8b5cb5979ca57a3b37
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-03 13:25:56 +0200

    execute array 8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -35,4 +35,5 @@ console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0 ? treu : false;
+console.log(isTeachersEmpty);
