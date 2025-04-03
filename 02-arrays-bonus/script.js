75738cb39d2dd30284babcc0271d94bcd9109765
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-03 13:38:31 +0200

    execute array bonus 1

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,22 +1,34 @@
-const teachers = [
-  'Nathan',
-  'Ed',
-  'Fabio',
-  'Phil',
-  'Carlo',
-  'Lewis',
-  'Luca'
-]; // NON MODIFICARE QUESTA VARIABILE
+const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
+console.log(teachers);
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [];
+for (let i = teachers.length - 1; i >= 0; i--) {
+  console.log(i);
+
+  const currentValue = teachers[i];
+  console.log(currentValue);
+
+  reversedTeachers.push(currentValue);
+}
+
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+// const longNames = [];
+// for (let i = 0; i < teachers.length; i++) {
+//   if (teachers[i].length >= 5) {
+//     longNames.push(teachers[i]);
+//   }
+// }
+
+// console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+// teachers.splice(teachers.indexOf("Ed"), 1);
+// console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
